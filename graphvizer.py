a388537f3b5f05749f08e362f809ec2aacfb7e32
Hao Lee <haolee.swjtu@gmail.com>
2017-12-10 11:43:39 +0800

    [New] Validate the dot file when user is inputting text
    The method to detect file syntax is inspired by API Documentation:
    
    set_syntax_file(syntax_file)
    
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- graphvizer.py
+++ graphvizer.py
@@ -1,12 +1,20 @@
 import sublime
 import sublime_plugin
 import functools
+import syntaxchecker
 
 
 # Trigged when user input text
 class UserEditListener(sublime_plugin.EventListener):
-	def on_modified_async(self, view):
-		pass
+	def on_modified(self, view):
+		# Detect language. Only process DOT file.
+		file_syntax = view.settings().get('syntax')
+		if "DOT.sublime-syntax" not in file_syntax:
+			return
+		# Get the contents of the whole file
+		region = sublime.Region(0, view.size())
+		contents = view.substr(region)
+		syntaxchecker.check(contents)
 
 #
 class GraphvizerCommand(sublime_plugin.TextCommand):
