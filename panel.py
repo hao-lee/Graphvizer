307e930c56becb4cd785d7ae65a1b4d62b68d904
Hao Lee <haolee.swjtu@gmail.com>
2017-12-23 11:41:23 +0800

    [Improved] Prettify the panel print
    Add datetime
    Change insert to append
    Provide a command for other modules to print text to panel
    
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- panel.py
+++ panel.py
@@ -1,18 +1,27 @@
 import sublime
 import sublime_plugin
+from datetime import datetime
 
 
+# Core utility class
 class Panel():
 	def __init__(self, window):
 		self.window = window
 		self.view = window.create_output_panel("graphvizer_panel")
 
 	def print(self, text):
-		self.view.run_command("select_all")
-		self.view.run_command("insert", {"characters": text})
-		self.window.run_command("show_panel", {"panel": "output.graphvizer_panel"})
+		#self.view.run_command("select_all")
+		dt = datetime.now()
+		dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
+		text =  "[%s] - %s\n" %(dt_str, text)
+		self.view.run_command("append", {"characters": text})
+		#self.window.run_command("show_panel", {"panel": "output.graphvizer_panel"})
 
-class TestPanelCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		panel = Panel(self.window)
-		panel.print("test")
+
+# Use this class as a window command to print text
+class PrintToGraphvizerPanelCommand(sublime_plugin.WindowCommand):
+	panel = None
+	def run(self, text):
+		if self.panel is None:
+			self.panel = Panel(self.window)
+		self.panel.print(text)
