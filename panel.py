bb53a1bab1c7057b4a9550556287105b804f91d0
Hao Lee <haolee.swjtu@gmail.com>
2017-12-23 16:37:36 +0800

    [Improved] Simplify the implementation of panel.py
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- panel.py
+++ panel.py
@@ -3,25 +3,15 @@ import sublime_plugin
 from datetime import datetime
 
 
-# Core utility class
-class Panel():
-	def __init__(self, window):
-		self.window = window
-		self.view = window.create_output_panel("graphvizer_panel")
-
-	def print(self, text):
+# Use this class as a window command to print text to graphvizer_panel
+class PrintToGraphvizerPanelCommand(sublime_plugin.WindowCommand):
+	view = None
+	def run(self, text):
+		if self.view is None:
+			self.view = self.window.create_output_panel("graphvizer_panel")
 		#self.view.run_command("select_all")
 		dt = datetime.now()
 		dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
 		text =  "[%s] - %s\n" %(dt_str, text)
 		self.view.run_command("append", {"characters": text})
 		#self.window.run_command("show_panel", {"panel": "output.graphvizer_panel"})
-
-
-# Use this class as a window command to print text
-class PrintToGraphvizerPanelCommand(sublime_plugin.WindowCommand):
-	panel = None
-	def run(self, text):
-		if self.panel is None:
-			self.panel = Panel(self.window)
-		self.panel.print(text)
