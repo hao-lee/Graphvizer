268cceca62828b7040484bb700a8a7119bcd40ff
Hao Lee <haolee.swjtu@gmail.com>
2017-12-23 17:53:17 +0800

    [Minor]: Delete useless commented lines
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- panel.py
+++ panel.py
@@ -9,10 +9,8 @@ class PrintToGraphvizerPanelCommand(sublime_plugin.WindowCommand):
 	def run(self, text):
 		if self.view is None:
 			self.view = self.window.create_output_panel("graphvizer_panel")
-		#self.view.run_command("select_all")
 		dt = datetime.now()
 		dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
 		text =  "[%s] - %s\n" %(dt_str, text)
 		self.view.run_command("append", {"characters": text})
 		self.view.run_command("move_to", {"to": "eof"})
-		#self.window.run_command("show_panel", {"panel": "output.graphvizer_panel"})
