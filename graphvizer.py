21f028ebd20ba6e6b8df75c0377b32a1baa09c85
Hao Lee <haolee.swjtu@gmail.com>
2017-12-24 15:18:32 +0800

    [New] Invoke dot command to render image
    When the dot content is valid, put it into a queue and a thread will get
    it and then use dot command to render image.
    
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- graphvizer.py
+++ graphvizer.py
@@ -5,10 +5,48 @@ import sys, os
 top_path = os.path.dirname(__file__) # Plugin directory
 sys.path.append(top_path)
 import syntaxchecker
+import threading, queue
+import subprocess
+import tempfile
 
 
 # Trigged when user input text
 class UserEditListener(sublime_plugin.EventListener):
+
+	def __init__(self):
+		# Start worker thread
+		self.queue = queue.Queue(maxsize=9)
+		thread = threading.Thread(target=self.dot_thread, daemon=True)
+		thread.start()
+
+	def dot_thread(self):
+		while True:
+			contents = self.queue.get(block=True, timeout=None)
+			'''
+			For purpose of cross-platform, we can't use TemporaryFile class because
+			subprocess can't read it directly on Windows. Using a regular file is a
+			good choice.
+			'''
+			tempdir = tempfile.gettempdir()
+			dot_file = os.path.join(tempdir, "graphvizer.dot")
+			with open(file=dot_file, mode="w", encoding="utf-8") as fd:
+				fd.write(contents)
+			image_file = os.path.join(tempdir, "graphvizer.png")
+			cmd = ["dot", dot_file, "-Tpng", "-o", image_file]
+			# For Windows, we must use startupinfo to hide the console window.
+			startupinfo = None
+			if os.name == "nt":
+				startupinfo = subprocess.STARTUPINFO()
+				startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+			process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+											stderr=subprocess.PIPE,
+											startupinfo=startupinfo)
+			stdout, stderr = process.communicate()
+			if len(stdout) != 0:
+				self.print(stdout)
+			if len(stderr) != 0:
+				self.print(stderr)
+
 	def on_modified(self, view):
 		# Detect language. Only process DOT file.
 		file_syntax = view.settings().get('syntax')
@@ -20,7 +58,7 @@ class UserEditListener(sublime_plugin.EventListener):
 		# Check if the syntax is valid
 		syntax_is_valid, error = syntaxchecker.check(contents)
 		if syntax_is_valid:
-			pass
+			self.queue.put(contents, block=True, timeout=None)
 		else:
 			self.print(error)
 
