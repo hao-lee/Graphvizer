becdf15779fb2c8d376cb44c764e1fbfde2cc6d3
Hao Lee <haolee.swjtu@gmail.com>
2017-12-24 21:42:29 +0800

    [Improved] Use the __init__ function to initialize member variable
    Let the super-class initialize the window object and then we just need to
    initialize the member variable self.view.
    The graphvizer_panel is now created in the __init__ function instead of in
    the run function. In this way, the graphvizer_panel can be created once the
    plugin has been loaded instead of waiting the first run.
    
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- panel.py
+++ panel.py
@@ -5,10 +5,12 @@ from datetime import datetime
 
 # Use this class as a window command to print text to graphvizer_panel
 class GraphvizerPrintToPanelCommand(sublime_plugin.WindowCommand):
-	view = None
+
+	def __init__(self, window):
+		super(GraphvizerPrintToPanelCommand, self).__init__(window)
+		self.view = self.window.create_output_panel("graphvizer_panel")
+
 	def run(self, text):
-		if self.view is None:
-			self.view = self.window.create_output_panel("graphvizer_panel")
 		dt = datetime.now()
 		dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
 		text =  "[%s] - %s\n" %(dt_str, text)
