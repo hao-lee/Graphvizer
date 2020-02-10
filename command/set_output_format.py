eb3e83cf4548812acdaca98558c1f64ad20d042a
Hao Lee <haolee.swjtu@gmail.com>
2020-02-10 14:27:58 +0800

    Support specifying image output format
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

---
+++ command/set_output_format.py
@@ -0,0 +1,29 @@
+import sublime
+import sublime_plugin
+
+
+class SetOutputFormatCommand(sublime_plugin.TextCommand):
+	def __init__(self, view):
+		super(SetOutputFormatCommand, self).__init__(view)
+		self.st_settings = sublime.load_settings("Graphvizer.sublime-settings")
+
+	# Called when menu is checked
+	def run(self, edit, output_format):
+		self.view.settings().set("output_format", output_format)
+
+	# Called when menu is shown. Used to determine whether a menu should be checked.
+	def is_checked(self, output_format):
+		# If menus are shown for the first time, we need to set the default engine.
+		if self.view.settings().get("output_format") is None:
+			self.view.settings().set("output_format", self.st_settings.get("default_output_format"))
+
+		if output_format == self.view.settings().get("output_format"):
+			return True
+		return False
+
+	def is_enabled(self):
+		return True
+		if self.view.settings().get("syntax") == "Packages/Graphviz/DOT.sublime-syntax":
+			return True
+		else:
+			return False
