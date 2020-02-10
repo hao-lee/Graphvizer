87e46bfc75641d6334d5a2a24d22b2f1c6c8bec5
Hao Lee <haolee.swjtu@gmail.com>
2020-02-10 14:27:58 +0800

    Support specifying image output format
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- command/__init__.py
+++ command/__init__.py
@@ -1,8 +1,10 @@
 from .graphvizer_panel import GraphvizerPrintToPanelCommand
 from .set_layout_engine import SetLayoutEngineCommand
+from .set_output_format import SetOutputFormatCommand
 
 
 __all__ = [
 	"GraphvizerPrintToPanelCommand",
-	"SetLayoutEngineCommand"
+	"SetLayoutEngineCommand",
+	"SetOutputFormatCommand"
 ]
